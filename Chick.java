6ee9137974f1575e388efe4360ff45592eb94dc4
unknown <giduong@697-334-Gandalf.sf.k12.internal>
2015-10-22 10:03:02 -0700

    hi

--- Chick.java
+++ Chick.java
@@ -1,16 +1,31 @@
 class Chick implements Animal {     
   private String myType;     
-  private String mySound;      
+  private String mySound, mySound2;      
   public Chick(String type, String sound) {         
     myType = type;         
     mySound = sound;
+    mySound2 = "";
   }     
+  public Chick(String type, String sound, String sound2) {         
+    myType = type;         
+    mySound = sound;
+    mySound2 = sound2;
+  }
   public Chick() {         
     myType = "unknown";         
     mySound = "unknown";
   }      
   public String getSound() {
-    return mySound;
+    //return mySound;
+    if (mySound2.isEmpty()) {
+      return mySound;
+    } else {
+      if (Math.random() > .5) {
+        return mySound;
+      } else {
+        return mySound2;
+      }
+    }
   }     
   public String getType() {
     return myType;
